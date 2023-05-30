4a16134082ee7a705a93a254137092dff6fbe6a8
vijoh01 <vijoh01@gmail.com>
2023-05-30 11:18:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,18 @@
+const log = document.querySelector(".login")
+const reg = document.querySelector(".registrera")
+
+const createBtn = document.querySelector(".createbutton")
+createBtn.addEventListener("click", function () {
+    log.style.display = "none"
+    reg.style.display = "block"
+});
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggleBtnIcon = document.querySelector('.toggle-btn i');
+const dropDownMenu = document.querySelector('.dropdown-menu');
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+};
